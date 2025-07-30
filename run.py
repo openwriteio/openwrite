95eec712adbe8ca3df6dc6f7382f06b0c6383b9d
openwrite <stuff@vwx.pl>
2025-07-30 23:07:05 +0200

    migrate

--- run.py
+++
@@ -1,7 +0,0 @@
-from openwrite import create_app
-app = create_app()
-
-if __name__ == "__main__":
-    import os
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host=os.environ.get("IP", "127.0.0.1"), port=int(os.environ.get("PORT", 5000)), use_reloader=True)
